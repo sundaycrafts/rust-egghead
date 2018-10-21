4e04c4e098b3630807e972290d89f8f45b3834b1
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-10-22 01:27:45 +0900

    Pass a JavaScript Function to WebAssembly and Invoke it from Rust
    https://egghead.io/lessons/javascript-pass-a-javascript-function-to-webassembly-and-invoke-it-from-rust

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,12 @@
+extern {
+  fn appendNumberToBody(x: u32);
+  fn alert(x: u32);
+}
+
 #[no_mangle]
-pub extern fn add_one(x: u32) -> u32 {
-    x + 1
+pub extern fn run() {
+  unsafe {
+    appendNumberToBody(42);
+    alert(4);
+  }
 }
